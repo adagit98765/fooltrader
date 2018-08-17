884d5d7c28d160fa928fd9146b2ecbc53efb0cb8
foolcage <5533061@qq.com>
2018-08-17 20:37:12 +0800

    tmp change

--- fooltrader/datarecorder/recorder.py
+++ fooltrader/datarecorder/recorder.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 
 
 class Recorder(object):
@@ -17,3 +18,20 @@ class Recorder(object):
 
     def record_kdata(self):
         pass
+
+    @staticmethod
+    def level_to_timeframe(level):
+        if level == 'day':
+            return '1d'
+        return level
+
+    @staticmethod
+    def evaluate_size_to_now(latest_record_timestamp, level='day'):
+        time_delta = pd.Timestamp.now() - latest_record_timestamp
+
+        if level == 'day':
+            return time_delta.days - 1
+        if level == '1m':
+            return time_delta.total_seconds() / 60
+        if level == '1h':
+            return time_delta.total_seconds() / (60 * 60)
